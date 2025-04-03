8485b027b005d9f39d0d583e965e73b1e16fdb62
CuongLiam <cuongdeptrai2706@gmail.com>
2025-04-03 10:45:39 +0700

    update

--- bai1/script.js
+++ bai1/script.js
@@ -1,17 +1,19 @@
 function filterNumbers(arr) {
-    if (!Array.isArray(arr) || arr.some(isNaN)) {
-        console.log("Dữ liệu không hợp lệ");
-        return;
-    }
-    
-    let result = arr.filter(num => num >= 10);
-    // function check(num){
-    //     // if(num >=10) return num;
-    //     return num>=10;
-    // }
-    console.log(result.length > 0 ? result : "Mảng không có phần tử nào");
+  if (!Array.isArray(arr) || arr.some(isNaN)) {
+    console.log("Dữ liệu không hợp lệ");
+    return;
+  }
+
+  let result = arr.filter((num, index) => num > 10)
+  // let result = arr.filter(num => num >= 10);
+
+//   let result = arr.filter(function (element, index) {
+//     return element > 10;
+//   });
+
+  console.log(result.length > 0 ? result : "Mảng không có phần tử nào");
 }
 
-filterNumbers([1, 22, 10, 9, 8]);
+filterNumbers([1, 22, 10, 9, 8,33,11,19]);
 filterNumbers([]);
 filterNumbers("abc");
